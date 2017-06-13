b6b80fd974015e6f096a499d3c9e1e30a19399d9
ben.micah.peterson <ben.micah.peterson@gmail.com>
2017-06-13 09:56:02 -0400

    Initial Example Data Binding Working

---
+++ PTC/app/product.controller.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app').controller('ProductController', ProductController);
+
+    function ProductController() {
+        var vm = this;
+
+        vm.product = {
+            ProductId: 1,
+            ProductName: 'Video Training'
+        };
+    }
+
+})();
