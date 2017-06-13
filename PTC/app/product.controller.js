8a47ad1adcb97f6479b9aae30eab36881b63a36f
ben.micah.peterson <ben.micah.peterson@gmail.com>
2017-06-13 15:24:57 -0400

    Added Click Event Handlers

--- PTC/app/product.controller.js
+++ PTC/app/product.controller.js
@@ -11,6 +11,11 @@
         vm.resetSearch = resetSearch;
         vm.searchImmediate = searchImmediate;
         vm.search = search;
+        vm.addClick = addClick;
+        vm.cancelClick = cancelClick;
+        vm.editClick = editClick;
+        vm.deleteClick = deleteClick;
+        vm.saveClick = saveClick;
 
         vm.products = [];
         vm.product = {
@@ -29,9 +34,55 @@
         };
 
 
+        const pageMode = {
+            LIST: 'List',
+            EDIT: 'Edit',
+            ADD: 'Add'
+        };
+
+        vm.uiState = {
+            mode: pageMode.LIST,
+            isDetailAreaVisible: false,
+            isListAreaVisible: true,
+            isSearchAreaVisuble: true,
+            isValid: true,
+            messages: []
+        };
+
+
         productList();
         searchCategoriesList();
 
+        function addClick() {
+            setUIState(pageMode.ADD)
+        }
+
+        function cancelClick() {
+            setUIState(pageMode.LIST);
+        }
+
+        function editClick(id) {
+            setUIState(pageMode.EDIT)
+        }
+
+        function deleteClick(id) {
+            if (confirm("Delete this Product?")) {
+
+            }
+        }
+
+        function saveClick() {
+            setUIState(pageMode.LIST);
+        }
+
+
+        function setUIState(state) {
+            vm.uiState.mode = state;
+
+            vm.uiState.isDetailAreaVisible = (state == pageMode.ADD || state == pageMode.EDIT);
+            vm.uiState.isListAreaVisible = (state == pageMode.LIST);
+            vm.uiState.isSearchAreaVisible = (state == pageMode.LIST);            
+        }
 
         function resetSearch() {
             vm.searchInput = {
@@ -59,6 +110,7 @@
                 searchEntity)
                 .then(function (result) {
                     vm.products = result.data;
+                    setUIState(pageMode.LIST);
                 }, function (error) {
                     handleException(error);
                 });
@@ -82,7 +134,9 @@
             dataService.get("/api/Product")
             .then(function (result) {
                 vm.products = result.data;
-                //debugger;
+
+
+                setUIState(pageMode.LIST);
                 },
                 function (error) {
                     handleException(error);
@@ -96,6 +150,7 @@
                     //debugger;
                 },
                 function (error) {
+                    vm.uiState.isValid = false;
                     handleException(error);
                 });
         }
